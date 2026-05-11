afb312d4f7735364ddb30df736cab5cf9fc31fea
samivulla <vempalli.subhan33@gmail.com>
2026-05-11 16:34:15 +0530

    added hellowrold example

---
+++ helloworldGraphql/server/server.js
@@ -0,0 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+    Query : {
+        hello: () => "Hello, World!"
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+console.log(`Server is running at ${url}`);
